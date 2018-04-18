66e2aff1c35fd1d94e84293de21937d8f101ae6c
KT <kazuhiro@freelinech.com>
2018-04-18 23:30:55 +0900

    node起動用のバッチファイルを修正、node設定ファイルのjsonにnodeのIPとportを記載する様変更
    NAT環境でのnode接続対応

--- bin/exec.bat
+++ bin/exec.bat
@@ -1,5 +1,13 @@
 
+set CMD=.\bin\CMD.bat
 set CMD_JAVA=.\java\jdk-10\bin\java
 
+set OPT_DRV_IE=.\drivers\IEDriverServer.exe
+set OPT_DRV_GK=.\drivers\geckodriver.exe
+set OPT_WEB_FF=C:\Program Files\Mozilla Firefox\firefox.exe
+
+set OPT_JAR=.\lib\selenium-server-standalone-3.9.1.jar
+set OPT_CNF=.\configs\NodeConfig.json
+
 cd %~p0\..\
-CALL "%CMD_JAVA%" -Dwebdriver.ie.driver=".\drivers\IEDriverServer.exe" -Dwebdriver.gecko.driver=".\drivers\geckodriver.exe" -Dwebdriver.firefox.bin="C:\Program Files\Mozilla Firefox\firefox.exe" -jar ".\lib\selenium-server-standalone-3.9.1.jar" -role node -nodeConfig ".\configs\NodeConfig.json"
+start "%CMD%" "%CMD_JAVA%" -Dwebdriver.ie.driver="%OPT_DRV_IE%" -Dwebdriver.gecko.driver="%OPT_DRV_GK%" -Dwebdriver.firefox.bin="%OPT_WEB_FF%" -jar "%OPT_JAR%" -role node -nodeConfig "%OPT_CNF%"
